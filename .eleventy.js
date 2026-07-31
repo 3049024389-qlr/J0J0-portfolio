5dd3d2bea9742f5d2bc9cea3894de27b4a9277d3
3049024389-qlr <3049024389@qq.com>
2026-08-01 01:15:45 +0800

    fix eleventy layout config and project styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function(eleventyConfig) {
-  // 把 css、js、images、admin 文件夹直接复制到输出目录
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
 
-  // 把 src/projects/ 里的文件收集成 projects 集合，按 order 排序
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/projects/*.md")
       .sort(function(a, b) {
@@ -17,7 +15,8 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes"
+      includes: "_includes",
+      layouts: "_includes"
     }
   };
 };
